7fe09b5271ae0b0400db85d7913d8fcdb4c1c18e
Sonish122 <sonishdhakal811@gmail.com>
2022-01-26 16:53:18 +0545

    index
    First projectt

---
+++ UI Design/Readme.txt
@@ -0,0 +1,5 @@
+The Following UI Files are created using Figma, and the ui design are inspired from WIX, nicepage. However no copyright images are taken from such sites.
+
+Commercial Use of these UI Files without the permission of the file owner will lead to a copyright violation.
+
+Copyright 2022 Suhana Khanal.
---
+++ java.js
@@ -0,0 +1,34 @@
+
+ /*
+var showme = document.getElementById('triggered');
+var removeme = document.getElementById('un-trigger');
+
+
+function show(){
+    showme.style.display='none';
+    removeme.style.display='block';
+}
+
+function hide(){
+    showme.style.display='block';
+    removeme.style.display-'hide';
+}
+
+*/
+
+
+var trigger = document.getElementById('buttons1')
+var back = document.getElementById('buttons2')
+
+
+function show(){
+    trigger.style.display ='none';
+    back.style.display='block';
+}
+
+function hide(){
+    trigger.style.display ='block';
+    back.style.display='none';
+}
+
+
